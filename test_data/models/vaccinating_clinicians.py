8da2712b95ccd0c4c07dada3b91ae2fd88e77782
root <root@FA-0817>
2024-06-07 13:19:06 +0100

    Fixed tests to run in dev

--- test_data/models/vaccinating_clinicians.py
+++ test_data/models/vaccinating_clinicians.py
@@ -1,6 +1,4 @@
 vaccinating_clinicians = [
     "Frazer Brindley - NMC (Nursing and Midwifery Council), NMC005",
-    "Neelima Guntupalli - GPhC (General Pharmaceutical Council), 8008",
     "Paul Parker - GMC (General Medical Council), GMC007",
-    "Scott Howells - HCPC (Health and Care Professions Council), FREEDOM"
 ]
