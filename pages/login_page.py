4e179690bfe3862e6474bd77dc53671fd3a49902
root <root@FA-0817>
2024-07-02 11:19:36 +0100

    Fix screenshots]

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,9 +1,13 @@
 from init_helpers import *
 
 LOGIN_BUTTON_ELEMENT = ("//button[text()='Log In']")
+ACCEPT_COOKIES_ELEMENT = ("#nhsuk-cookie-banner__link_accept_analytics")
 
 def navigate_to_ravs_login_page(url):
     navigate_to_url(url)
+    wait_for_element_to_appear(ACCEPT_COOKIES_ELEMENT)
+    if check_element_exists(ACCEPT_COOKIES_ELEMENT):
+        find_element_and_perform_action(ACCEPT_COOKIES_ELEMENT, "click")
     wait_for_page_to_load()
 
 def check_login_button_exists():
