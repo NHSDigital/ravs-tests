ee1171554d6a416baac547ab525245a70cac8724
neelimaguntupalli1-nhs <neelimaguntupalli1-nhs@users.noreply.github.com>
2024-08-22 22:15:38 +0000

    deploy: 800bc43bf726fa5836c5ed33a6d0f8e834360044

---
+++ test_data/models/pertussis_consent_vaccine_radio_button_xpath_map.py
@@ -0,0 +1,5 @@
+pertussis_consent_vaccine_radio_button_xpath_map = {
+        "adacel vaccine suspension": "//input[@class='nhsuk-radios__input' and @name='ConsentVaccineId' and @value='15']",
+        "boostrix-IPV suspension": "//input[@class='nhsuk-radios__input' and @name='ConsentVaccineId' and @value='31']",
+        "repevax vaccine suspension ": "//input[@class='nhsuk-radios__input' and @name='ConsentVaccineId' and @value='33']"
+    }
---
+++ test_data/models/pertussis_vaccine_types.py
@@ -0,0 +1,5 @@
+pertussis_vaccine_types = [
+    "Adacel vaccine suspension",
+    "Boostrix-IPV suspension",
+    "Repevax vaccine suspension"
+]
