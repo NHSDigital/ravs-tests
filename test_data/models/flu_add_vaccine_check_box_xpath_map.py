65a77ab53ac4b0ecfde5e5affef404b451467450
colinpiper3 <colinpiper3@users.noreply.github.com>
2024-09-10 06:29:04 +0000

    deploy: 79a4f7c5dcc5c924f251b070bcabb9ca9e41eb21

--- test_data/models/flu_add_vaccine_check_box_xpath_map.py
+++ test_data/models/flu_add_vaccine_check_box_xpath_map.py
@@ -1,10 +1,10 @@
 flu_add_vaccine_check_box_xpath_map = {
-        "fluenz tetra - laiv": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='6']",
-        "quadrivalent influenza vaccine - qive": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='7']",
-        "quadrivalent influvac sub - unit tetra - qive": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='8']",
-        "flucelvax tetra - qivc": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='9']",
-        "supemtek - qivr": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='10']",
-        "fluad tetra - aqiv": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='11']",
-        "cell-based quadrivalent - qivc": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='12']",
-        "adjuvanted quadrivalent - aqiv": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='13']"
+        "Quadrivalent Influenza vaccine - QIVe": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='7']",
+        "Flucelvax Tetra - QIVc": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='9']",
+        "Supemtek - QIVr": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='10']",
+        "Cell-based Quadrivalent - QIVc": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='12']",
+        "Adjuvanted Quadrivalent - aQIV": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='13']",
+        "Fluenz LAIV – Astra Zeneca": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='23']",
+        "Influvac sub-unit Tetra (QIVe surface antigen) - Viatri": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='24']",
+        "Quadrivalent Influenza Vaccine (Split Virion, Inactivated) High-Dose – QIV-HD - Sanofi": "//input[@class='nhsuk-checkboxes__input' and @name='FluVaccineIds' and @value='25']"
     }
