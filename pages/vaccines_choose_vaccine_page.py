49e30d50b34f680a3cd7f0b57bb755ae28675977
neelimaguntupalli1-nhs <neelimaguntupalli1-nhs@users.noreply.github.com>
2024-10-08 17:31:10 +0000

    deploy: 82566e8d82fb3b3967aef06d9091e6c648ebfd36

--- pages/vaccines_choose_vaccine_page.py
+++ pages/vaccines_choose_vaccine_page.py
@@ -2,13 +2,13 @@ from init_helpers import *
 
 CONTINUE_BUTTON=("//button[text()='Continue']")
 
-def click_continue_button():
-    find_element_and_perform_action(CONTINUE_BUTTON, "click")
+# def click_continue_button():
+#     find_element_and_perform_action(CONTINUE_BUTTON, "click")
 
-def click_vaccine_radiobutton(vaccine):
-    element = (f"//label[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '{vaccine.lower()}')]/preceding-sibling::input[@name='VaccineProgramId']")
-    find_element_and_perform_action(element, "click")
+# def click_vaccine_radiobutton(vaccine):
+#     element = (f"//label[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '{vaccine.lower()}')]/preceding-sibling::input[@name='VaccineProgramId']")
+#     find_element_and_perform_action(element, "click")
 
-def click_vaccine_type_radiobutton(vaccine_type):
-    element = (f"//label[contains(text(), '{vaccine_type}')]/preceding-sibling::input[@name='VaccineId']")
-    find_element_and_perform_action(element, "click")
+# def click_vaccine_type_radiobutton(vaccine_type):
+#     element = (f"//label[contains(text(), '{vaccine_type}')]/preceding-sibling::input[@name='VaccineId']")
+#     find_element_and_perform_action(element, "click")
