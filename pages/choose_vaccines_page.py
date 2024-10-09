9814d7be25c6dbe2527c8540b4bd3abb89343693
neelimaguntupalli1-nhs <neelimaguntupalli1-nhs@users.noreply.github.com>
2024-10-09 12:58:35 +0000

    deploy: 4e8395788f3fc57ce134160c9367d2c435bdc7c6

--- pages/choose_vaccines_page.py
+++ pages/choose_vaccines_page.py
@@ -12,48 +12,47 @@ AGE_BASED_WARNING = ("text", "This vaccine may not be recommended for a person o
 MIN_INTERVAL_BASED_WARNING = ("text", "You may have not reached the minimal interval between COVID-19 vaccine doses for this patient. This could depend on the clinical circumstances. For vaccination guidance, visit ")
 
 def click_site_radiobutton(site):
-    element = get_element_by_type("label", site)
-    find_element_with_locator_and_perform_action(element, "check")
+    element = ("label", site)
+    click_element(element)
 
 def click_back_button_choosing_vaccine_for_patient():
-    element = get_element_by_type(BACK_ELEMENT)
-    find_element_with_locator_and_perform_action(element, "click")
+    click_element(BACK_ELEMENT)
 
 def click_delivery_team_radiobutton(deliveryTeam):
-    element = get_element_by_type("label", deliveryTeam)
-    if element:
-        find_element_with_locator_and_perform_action(element, "check")
+    element = ("label", deliveryTeam)
+    if check_if_element_exists(element, True) == True:
+        check_element(element)
     else:
         print("Delivery team not available at organization")
 
 def click_vaccine_type_radiobutton(vaccine_type):
-    element = get_element_by_type("label", vaccine_type)
-    if element:
-        find_element_with_locator_and_perform_action(element, "check")
+    element = ("label", vaccine_type)
+    if check_if_element_exists(element, True) == True:
+        check_element(element)
     else:
         print("Vaccine type not available")
 
 def click_vaccine_radiobutton(vaccine):
-    element = get_element_by_type("label", vaccine)
-    if element:
-        find_element_with_locator_and_perform_action(element, "check")
+    element = ("label", vaccine)
+    if check_if_element_exists(element, True) == True:
+        check_element(element)
     else:
         print("Vaccine not available")
 
 def check_back_button_exists():
-    return check_element_by_locator_exists(get_element_by_type(*BACK_ELEMENT), True)
+    return check_if_element_exists(BACK_ELEMENT, True)
 
 def check_age_based_warning_exists():
-    return check_element_by_locator_exists(get_element_by_type(*AGE_BASED_WARNING), False)
+    return check_if_element_exists(AGE_BASED_WARNING, False)
 
 def check_minimum_interval_based_warning_exists():
-    return check_element_by_locator_exists(get_element_by_type(*MIN_INTERVAL_BASED_WARNING), False)
+    return check_if_element_exists(MIN_INTERVAL_BASED_WARNING, False)
 
 def check_covid_radiobutton_exists():
-    return check_element_exists(COVID_RADIOBUTTON, True)
+    return check_if_element_exists(COVID_RADIOBUTTON, True)
 
 def check_flu_radiobutton_exists():
-    return check_element_exists(COVID_RADIOBUTTON, True)
+    return check_if_element_exists(FLU_RADIOBUTTON, True)
 
 def click_continue_to_assess_patient_button():
-    find_element_with_locator_and_perform_action(get_element_by_type(*CONTINUE_BUTTON), "click")
+    click_element(CONTINUE_BUTTON)
