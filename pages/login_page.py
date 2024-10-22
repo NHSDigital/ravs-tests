f16ad0eee0cb431a84c4b48ad8933cb371f2cd96
neelimaguntupalli1-nhs <neelimaguntupalli1-nhs@users.noreply.github.com>
2024-10-22 11:29:02 +0000

    deploy: f776266a53e68c886c8629f18ae1c1f88171518e

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,16 +1,19 @@
+import time
 from init_helpers import *
 
-LOGIN_BUTTON_ELEMENT = ("//button[text()='Log In']")
-ACCEPT_COOKIES_ELEMENT = ("#nhsuk-cookie-banner__link_accept_analytics")
+LOGIN_BUTTON_ELEMENT = ("role", "button", "Log In")
+ACCEPT_COOKIES_ELEMENT = ("role", "button", "I'm ok with analytics cookies")
 
 def navigate_to_ravs_login_page(url):
     navigate_to_url(url)
-    wait_for_element_to_appear(ACCEPT_COOKIES_ELEMENT)
-    if check_element_exists(ACCEPT_COOKIES_ELEMENT):
+    wait_for_element_to_appear(LOGIN_BUTTON_ELEMENT)
+    if check_element_exists(ACCEPT_COOKIES_ELEMENT, False) is True:
         find_element_and_perform_action(ACCEPT_COOKIES_ELEMENT, "click")
 
 def check_login_button_exists():
+    wait_for_element_to_appear(LOGIN_BUTTON_ELEMENT)
     return check_element_exists(LOGIN_BUTTON_ELEMENT, True)
 
 def click_login_button():
+    wait_for_element_to_appear(LOGIN_BUTTON_ELEMENT)
     find_element_and_perform_action(LOGIN_BUTTON_ELEMENT, "click")
