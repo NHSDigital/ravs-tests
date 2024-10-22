bac89ac13d41514ddd45e313c0743648990d6e6f
neelimaguntupalli1-nhs <neelimaguntupalli1-nhs@users.noreply.github.com>
2024-10-22 16:50:18 +0000

    deploy: 5f34f2fce01ca17ca98030bad47bbd20c55566e4

--- tests/test_find_a_patient_steps.py
+++ tests/test_find_a_patient_steps.py
@@ -1,3 +1,4 @@
+import warnings
 from pytest_bdd import given, when, then, scenario, scenarios
 from pytest_bdd.parsers import parse
 from pages.vaccinator_location_page import *
@@ -10,12 +11,20 @@ from conftest import *
 from faker import Faker
 import random
 
-fake = Faker('en_GB')
+warnings.filterwarnings("ignore", category=UserWarning, module='faker')
+
+
 
 features_directory = get_working_directory() + "features"
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.CRITICAL)
 logger = logging.getLogger(__name__)
+
+logging.getLogger('faker').setLevel(logging.CRITICAL)
+logging.getLogger('faker.factory').setLevel(logging.CRITICAL)
+logging.getLogger('faker.providers').setLevel(logging.CRITICAL)
+
+fake = Faker('en_GB')
 
 @pytest.fixture(scope='function')
 def shared_data():
