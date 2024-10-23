9f0135eb8f8a3b3e43dda52b50de69e40ae07e71
neelimaguntupalli1-nhs <neelimaguntupalli1-nhs@users.noreply.github.com>
2024-10-23 09:54:32 +0000

    deploy: 97045087fe3f825499aca0f04913bbc6bdd92c83

--- pages/nhs_signin_page.py
+++ pages/nhs_signin_page.py
@@ -1,39 +1,54 @@
+import time
 from init_helpers import *
 
-SIGN_IN_BUTTON_ELEMENT = ("//input[@value='Sign in']")
-KEEP_ME_SIGNED_IN_CHECKBOX_ELEMENT = ("//label[text()='Keep me signed in']")
-EMAIL_INPUT_ELEMENT = ("#input28")
-PASSWORD_INPUT_ELEMENT = ("#input36")
+SIGN_IN_BUTTON_ELEMENT = ("role", "button", "Sign in")
+KEEP_ME_SIGNED_IN_CHECKBOX_ELEMENT = ("text", "Keep me signed in")
+EMAIL_INPUT_ELEMENT = ("label", "Email")
+PASSWORD_INPUT_ELEMENT = ("label", "Password")
 ALERT_TEXT_EMAILADDRESS = ("//p[contains(@id, 'input-container-error')]")
 ALERT_TEXT_PASSWORD = ("//p[contains(@id, 'input-container-error')]")
-ERROR_UNABLE_TO_SIGN_IN = ("//p[text()='Unable to sign in']")
-FOUND_SOME_ERRORS = ("//p[text()='We found some errors. Please review the form and make corrections.']")
+ERROR_UNABLE_TO_SIGN_IN = ("text", "Unable to sign in")
+FOUND_SOME_ERRORS = ("text", "We found some errors. Please review the form and make corrections.")
+LOGOUT_NAV_ELEMENT = ("role", "link", "Log Out")
 
 def navigate_to_nhs_signin_page(url):
     navigate_to_url(url)
 
 def check_signin_button_exists():
+    wait_for_element_to_appear(EMAIL_INPUT_ELEMENT)
+    wait_for_element_to_appear(PASSWORD_INPUT_ELEMENT)
+    wait_for_element_to_appear(SIGN_IN_BUTTON_ELEMENT)
     return check_element_exists(SIGN_IN_BUTTON_ELEMENT, True)
 
 def click_nhs_signin_button():
+    wait_for_element_to_appear(EMAIL_INPUT_ELEMENT)
+    wait_for_element_to_appear(PASSWORD_INPUT_ELEMENT)
+    wait_for_element_to_appear(SIGN_IN_BUTTON_ELEMENT)
     find_element_and_perform_action(SIGN_IN_BUTTON_ELEMENT, "click")
+    wait_for_page_to_load(timeout=10)
+    wait_for_element_to_appear(LOGOUT_NAV_ELEMENT)
 
 def enter_email_address(emailAddress):
+    wait_for_element_to_appear(EMAIL_INPUT_ELEMENT)
     find_element_and_perform_action(EMAIL_INPUT_ELEMENT, "input_text", emailAddress)
 
 def clear_emailAddress():
-    clear_element(EMAIL_INPUT_ELEMENT)
+    wait_for_element_to_appear(EMAIL_INPUT_ELEMENT)
+    find_element_and_perform_action(EMAIL_INPUT_ELEMENT, "clear")
 
 def clear_password():
-    clear_element(PASSWORD_INPUT_ELEMENT)
+    wait_for_element_to_appear(PASSWORD_INPUT_ELEMENT)
+    find_element_and_perform_action(PASSWORD_INPUT_ELEMENT, "clear")
 
 def enter_password(password):
+    wait_for_element_to_appear(PASSWORD_INPUT_ELEMENT)
     find_element_and_perform_action(PASSWORD_INPUT_ELEMENT, "input_text", password)
 
 def click_keep_me_signedin():
-    find_element_and_perform_action(KEEP_ME_SIGNED_IN_CHECKBOX_ELEMENT, "click_checkbox")
+    find_element_and_perform_action(KEEP_ME_SIGNED_IN_CHECKBOX_ELEMENT, "click")
 
 def check_password_error_alert_exists():
+    wait_for_page_to_load()
     return check_element_exists(ALERT_TEXT_PASSWORD, True)
 
 def check_emailAddress_error_alert_exists():
