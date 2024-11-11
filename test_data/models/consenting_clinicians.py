14dfea600fa18af83ad0d506267514707b6ce654
root <root@FA-0817>
2024-11-11 16:49:50 +0000

    Fixed failed RSV and pertussis tests

--- test_data/models/consenting_clinicians.py
+++ test_data/models/consenting_clinicians.py
@@ -1,5 +1,5 @@
 consenting_clinicians = [
     "Mark Chapman - mark.chapman1@nhs.net",
-    "Test Clinicians - test@nhs.net",
+    "Test Clinicians - Test@nhs.net",
     "Neelima Guntupalli - neelima.guntupalli1@nhs.net"
 ]
