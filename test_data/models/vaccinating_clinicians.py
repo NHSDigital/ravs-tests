d921be0d75280c57a8e5c81fd4a295f525fb913d
root <root@FA-0817>
2024-11-11 13:09:12 +0000

    Fixed -pl=,'@~

--- test_data/models/vaccinating_clinicians.py
+++ test_data/models/vaccinating_clinicians.py
@@ -1,8 +1,5 @@
 vaccinating_clinicians = [
     "Mark Chapman - mark.chapman1@nhs.net",
     "Test Clinicians - test@nhs.net",
-    "Test Clinicians - test@nhs.net",
-    "Test Clinicians - test@nhs.net",
-    "Paul Parker - paul.parker13@nhs.net",
-    "Richard Williams - rich.williams1@nhs.net"
+    "Neelima Guntupalli - neelima.guntupalli1@nhs.net"
 ]
