36c4d00f5986768b7df2eb1145001c9183f9d23b
neelimaguntupalli1-nhs <neelimaguntupalli1-nhs@users.noreply.github.com>
2024-11-13 13:57:52 +0000

    deploy: af6bd6d7321c187acbbed64320397667f41acf7e

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,7 @@ from init_helpers import *
 
 LOGIN_BUTTON_ELEMENT = ("role", "button", "Log In")
 ACCEPT_COOKIES_ELEMENT = ("role", "button", "I'm ok with analytics cookies")
+PAGE_LOADING_ELEMENT = ("text", "Loading...Loading...")
 
 def navigate_to_ravs_login_page(url):
     navigate_to_url(url)
@@ -11,9 +12,11 @@ def navigate_to_ravs_login_page(url):
         find_element_and_perform_action(ACCEPT_COOKIES_ELEMENT, "click")
 
 def check_login_button_exists():
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     wait_for_element_to_appear(LOGIN_BUTTON_ELEMENT)
     return check_element_exists(LOGIN_BUTTON_ELEMENT, True)
 
 def click_login_button():
     wait_for_element_to_appear(LOGIN_BUTTON_ELEMENT)
     find_element_and_perform_action(LOGIN_BUTTON_ELEMENT, "click")
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
