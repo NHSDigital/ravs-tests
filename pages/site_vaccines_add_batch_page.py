0e2a95b43397cd8a6069663a9674a8f795fbd4fa
neelimaguntupalli1-nhs <neelima.guntupalli1@nhs.net>
2024-11-14 18:32:02 +0000

    Ravs 1094 automate reports feature (#34)
    * Added intial test for creating reports feature
    
    * Fixed failing test for RSV
    
    * Added more tests
    
    * Fixed -pl=,'@~
    
    * Fixed failed RSV and pertussis tests
    
    * Fixed failed RSV and pertussis tests
    
    * Updated duplicate batch error and fixed non reponsive page issues when switching from vaccines to patient search
    
    * Get sequential index instead of random index
    
    * Fixed failing RSV and pertussis tests by adding new batches tagged as Automation
    
    * Fixed failing duplicate batch test following fix of 17456
    
    * Reduced time.sleep to see how tests fare
    
    * Reset back time and reduced them slightly
    
    * Increaed wait time for logout tests
    
    * Increaed wait time for logout tests
    
    * Increaed wait time for logout tests
    
    * Added 2 new tests to reports
    
    * Fixed add batches tests
    
    ---------
    
    Co-authored-by: root <root@FA-0817>

--- pages/site_vaccines_add_batch_page.py
+++ pages/site_vaccines_add_batch_page.py
@@ -1,11 +1,12 @@
 from init_helpers import *
 
-VACCINE_BATCH_NUMBER_INPUT = ("#OtherBatchNumber")
-BATCH_NUMBER_PREFIX_INPUT = ("#CovidBatchNumberPrefix")
-BATCH_NUMBER_SUFFIX_INPUT = ("#CovidBatchNumberSuffix")
-EXPIRY_DAY_INPUT = ("#ExpiryDate_1")
-EXPIRY_MONTH_INPUT = ("#ExpiryDate_2")
-EXPIRY_YEAR_INPUT = ("#ExpiryDate_3")
+ADD_BATCH_BUTTON = ("role", "link", "Add batch")
+BACK_BUTTON_ON_VACCINE_BATCHES_PAGE = ("role", "link", "Back", True)
+CONTINUE_TO_CHECK_AND_CONFIRM_VACCINE_BATCH_BUTTON = ("role", "button", "Continue")
+BATCH_NUMBER_INPUT_FIELD = ("label", "Batch number")
+EXPIRY_DATE_DAY_INPUT_FIELD = ("placeholder", "DD")
+EXPIRY_DATE_MONTH_INPUT_FIELD = ("placeholder", "MM")
+EXPIRY_DATE_YEAR_INPUT_FIELD = ("placeholder", "YYYY")
 ADD_BATCH_TITLE = ("role", "heading", "Add batch")
 CONTINUE_TO_CONFIRM_BATCH_BUTTON = ("role", "button", "Continue")
 ERROR_MESSAGE_BATCH_ALREADY_EXISTS = ("text", "Error: There is already a batch with this number and expiry date")
@@ -14,6 +15,10 @@ ERROR_MESSAGE_ENTER_THE_BATCH_EXPIRY_DATE = ("text", "Error: Enter the expiry da
 ERROR_MESSAGE_LINK_BATCH_ALREADY_EXISTS = ("role", "button", "There is already a batch with this number and expiry date")
 ERROR_MESSAGE_LINK_ENTER_THE_BATCH_NUMBER = ("role", "button", "Enter the batch number")
 ERROR_MESSAGE_LINK_ENTER_THE_BATCH_EXPIRY_DATE = ("role", "button", "Enter the expiry date")
+
+def click_add_batch_button():
+    wait_for_element_to_appear(ADD_BATCH_BUTTON)
+    find_element_and_perform_action(ADD_BATCH_BUTTON, "click")
 
 def check_batch_already_exists_error_message_is_displayed():
     wait_for_element_to_appear(ERROR_MESSAGE_BATCH_ALREADY_EXISTS)
@@ -48,25 +53,21 @@ def check_add_batch_title_exists(wait):
     return check_element_exists(ADD_BATCH_TITLE, wait)
 
 def enter_batch_number(batch_number):
-    wait_for_element_to_appear(VACCINE_BATCH_NUMBER_INPUT)
-    if check_element_exists(VACCINE_BATCH_NUMBER_INPUT, False):
-        find_element_and_perform_action(VACCINE_BATCH_NUMBER_INPUT, "input_text", batch_number)
-    else:
-        prefix, suffix = batch_number.split('-')
-        find_element_and_perform_action(BATCH_NUMBER_PREFIX_INPUT, "input_text", prefix)
-        find_element_and_perform_action(BATCH_NUMBER_SUFFIX_INPUT, "input_text", suffix)
+    wait_for_element_to_appear(BATCH_NUMBER_INPUT_FIELD)
+    if check_element_exists(BATCH_NUMBER_INPUT_FIELD, False):
+        find_element_and_perform_action(BATCH_NUMBER_INPUT_FIELD, "input_text", batch_number)
 
 def enter_vaccine_batch_number(batch_number):
-    wait_for_element_to_appear(VACCINE_BATCH_NUMBER_INPUT)
-    find_element_and_perform_action(VACCINE_BATCH_NUMBER_INPUT, "input_text", batch_number)
+    wait_for_element_to_appear(BATCH_NUMBER_INPUT_FIELD)
+    find_element_and_perform_action(BATCH_NUMBER_INPUT_FIELD, "input_text", batch_number)
 
 def enter_expiry_date(expiry_date):
     try:
         day, month, year = expiry_date.split('/')
-        wait_for_element_to_appear(EXPIRY_DAY_INPUT)
-        find_element_and_perform_action(EXPIRY_DAY_INPUT, "input_text", day)
-        find_element_and_perform_action(EXPIRY_MONTH_INPUT, "input_text", month)
-        find_element_and_perform_action(EXPIRY_YEAR_INPUT, "input_text", year)
+        wait_for_element_to_appear(EXPIRY_DATE_DAY_INPUT_FIELD)
+        find_element_and_perform_action(EXPIRY_DATE_DAY_INPUT_FIELD, "input_text", day)
+        find_element_and_perform_action(EXPIRY_DATE_MONTH_INPUT_FIELD, "input_text", month)
+        find_element_and_perform_action(EXPIRY_DATE_YEAR_INPUT_FIELD, "input_text", year)
     except ValueError:
         raise ValueError("Invalid expiry date format. Please use the format 'dd/MM/yyyy'.")
 
