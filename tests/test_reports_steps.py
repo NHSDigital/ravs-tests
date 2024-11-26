9ee0bd5a5bbdf8e2bb1c674d4dc70f4d3d4d4d58
root <root@FA-0817>
2024-11-26 19:07:35 +0000

    Added new tests

--- tests/test_reports_steps.py
+++ tests/test_reports_steps.py
@@ -6,6 +6,8 @@ from pytest_bdd.parsers import parse
 from pages.login_page import *
 from pages.home_page import *
 from pages.nhs_signin_page import *
+from pages.reports_check_and_confirm_page import *
+from pages.reports_data_selection_page import *
 from pages.reports_data_selection_page import *
 from pages.reports_date_range_selection_page import *
 import logging
@@ -175,8 +177,42 @@ def I_select_vaccinetype_and_click_continue(shared_data, site):
     attach_screenshot("click_continue_to_reports_select_data_button")
     logging.info("click_continue_to_reports_select_data_button")
 
+@then("the choose data page should be displayed and all data options should be checked by default")
+def the_choose_data_page_should_be_displayed():
+    assert check_reports_data_check_box_exists("Patients") == True
+    assert check_reports_data_check_box_checked("Patients") == True
+    assert check_reports_data_check_box_checked("Staff") == True
+    assert check_reports_data_check_box_checked("Site or delivery team") == True
+    assert check_reports_data_check_box_checked("Assessment and consent") == True
+    assert check_reports_data_check_box_checked("Vaccination") == True
+    attach_screenshot("check_choose_data_pages_reports_exists")
+    logging.info("check_choose_data_pages_reports_exists")
+
+@when(parse('I click continue on the data page'))
+def I_select_data_and_click_continue(shared_data):
+    click_continue_to_reports_select_data_button()
+    attach_screenshot("click_continue_to_reports_select_data_button")
+    logging.info("click_continue_to_reports_select_data_button")
+
+@then("the check and confirm page should be displayed")
+def the_check_and_confirm_page_should_be_displayed():
+    assert check_reports_change_data_button_exists() == True
+    attach_screenshot("check_reports_change_data_pages_reports_exists")
+    logging.info("check_reports_change_data_pages_reports_exists")
+    attach_screenshot("check_choose_sites_page_is_displayed")
+    logging.info("check_choose_sites_page_is_displayed")
+
+@when(parse('I select the site {site} and click continue'))
+def I_select_vaccinetype_and_click_continue(shared_data, site):
+    check_site_check_box(site)
+    attach_screenshot("click_" + site.lower() + "_check_box_on_reports_page")
+    logging.info("click_" + site.lower() + "_check_box_on_reports_page")
+    click_continue_to_reports_select_data_button()
+    attach_screenshot("click_continue_to_reports_select_data_button")
+    logging.info("click_continue_to_reports_select_data_button")
+
 @then("the choose data page should be displayed")
 def the_choose_data_page_should_be_displayed():
-    assert check_data_check_box_exists("Patients") == True
+    assert check_reports_data_check_box_exists("Patients") == True
     attach_screenshot("check_choose_data_pages_reports_exists")
     logging.info("check_choose_data_pages_reports_exists")
