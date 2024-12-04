4818767686f8ac7adcf3068a8a72d8cee1848604
root <root@FA-0817>
2024-12-04 17:50:11 +0000

    Added screenshot after checking vaccination history label

--- conftest.py
+++ conftest.py
@@ -498,6 +498,7 @@ def step_search_for_patient(shared_data, name):
 def step_choose_vaccine_and_vaccine_type(shared_data, chosen_vaccine, batch_number, batch_expiry_date):
     time.sleep(3)
     assert check_vaccine_history_not_available_label_element_exists() == False
+    attach_screenshot("checked_vaccine_history_not_available_label_element_exists")
     immunisation_history_records_count_before_vaccination = click_on_patient_search_result_and_click_choose_vaccine(shared_data['patient_name'], chosen_vaccine)
     shared_data["immunisation_history_records_count_before_vaccination"] = immunisation_history_records_count_before_vaccination
     choose_vaccine_and_vaccine_type_for_patient(shared_data['site'], chosen_vaccine, shared_data['chosen_vaccine_type'])
