b5214e198b3798fe5c02cac544cc10c07553e5ac
root <root@FA-0817>
2024-12-11 11:53:03 +0000

    Increase timeout for waiting

--- pages/patient_details_page.py
+++ pages/patient_details_page.py
@@ -28,8 +28,9 @@ PAGE_LOADING_ELEMENT = ("text", "Loading...Loading...")
 VACCINATION_HISTORY_NOT_AVAILABLE = ("role", "heading", "No vaccination history available")
 
 def check_vaccine_history_not_available_label_element_exists():
+    time.sleep(2)
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
-    time.sleep(4)
+    time.sleep(3)
     wait_for_element_to_appear(CHOOSE_VACCINE_BUTTON)
     return check_element_exists(VACCINATION_HISTORY_NOT_AVAILABLE)
 
