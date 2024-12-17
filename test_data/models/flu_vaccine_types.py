769e7ac8bc2a8a738f716c3c32535e7293ae57cf
root <root@FA-0817>
2024-12-17 12:10:46 +0000

    Fix failing tests after new vaccine type addition

--- test_data/models/flu_vaccine_types.py
+++ test_data/models/flu_vaccine_types.py
@@ -9,7 +9,7 @@ flu_vaccine_types = [
     # "Adjuvanted Quadrivalent - aQIV"
     "Quadrivalent Influenza Vaccine",
     "Cell-based Quadrivalent Influenza Vaccine",
-    "Adjuvanted Quadrivalent Influenza Vaccine"
+    "Adjuvanted Quadrivalent Influenza Vaccine",
     "Fluenz (LAIV)",
     "Influenza Tetra MYL",
     "Quadrivalent Influenza Vaccine – High Dose",
