9f90207606bc7900e90675816be777786bd3b9fb
neelimaguntupalli1-nhs <neelima.guntupalli1@nhs.net>
2024-12-23 18:42:54 +0000

    Ravs 1228 automate user management (#53)
    * git resolve issue with Bobby tickle test failure
    
    * Added new pages for manage users
    
    * Added new vaccine type test
    
    * Fix failing tests after new vaccine type addition
    
    * Fix failing tests after new vaccine type addition
    
    * Added new test for 1368
    
    * removed png from PR commit
    
    * Added page elements for manage users
    
    * Fix issue with all tests failing
    
    * Zoom to element for capturing screenshot
    
    * Zoom to element for capturing screenshot
    
    * Improved test execution time by fixing the issue when adding vaccine and batch to site
    
    * Removed comments
    
    * Added more screenshots to age based warning tests
    
    * Added more screenshots to age based warning tests
    
    * Added new tests for user management
    
    * Added new tests for user management
    
    * Added new tests for user management
    
    * Added new tests for user management
    
    ---------
    
    Co-authored-by: root <root@FA-0817>

--- pages/manage_users_deactivate_users_page.py
+++ pages/manage_users_deactivate_users_page.py
@@ -1,6 +1,9 @@
 from init_helpers import *
 
 PAGE_LOADING_ELEMENT = ("text", "Loading...Loading...")
+BACK_TO_MANAGE_USERS_LINK = ("role", "button", "Back to Manage users")
+DEACTIVATED_USERS_HEADING = ("role", "heading", "Deactivated users")
+DEACTIVATED_USERS_LIST_TABLE = ("xpath", '//table[contains(@class, "nhsuk-table")]')
 
 def check_deactivated_user_exists_in_list(user_email_address):
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
@@ -12,3 +15,45 @@ def click_reactivate_user_link(user_email_address):
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     element = ("xpath", f'//td[@class="nhsuk-table__cell" and text()="{user_email_address}"]/following-sibling::td[@class="nhsuk-table__cell"]/a[text()="Reactivate"]')
     find_element_and_perform_action(element, "click")
+
+def click_back_to_manage_users_link():
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
+    wait_for_element_to_appear(BACK_TO_MANAGE_USERS_LINK)
+    find_element_and_perform_action(BACK_TO_MANAGE_USERS_LINK, "click")
+
+def check_deactivated_users_list_table_exists():
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
+    wait_for_element_to_appear(BACK_TO_MANAGE_USERS_LINK)
+    wait_for_element_to_appear(DEACTIVATED_USERS_HEADING)
+    return check_element_exists(DEACTIVATED_USERS_LIST_TABLE)
+
+def check_deactivated_users_page_heading_exists():
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
+    wait_for_element_to_appear(BACK_TO_MANAGE_USERS_LINK)
+    wait_for_element_to_appear(DEACTIVATED_USERS_HEADING)
+    return check_element_exists(DEACTIVATED_USERS_HEADING)
+
+def get_first_deactivated_users_name():
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
+    element = ("xpath", '(//a[text()="Reactivate"]/ancestor::tr/td[1])[1]')
+    if check_element_exists(element):
+        return find_element_and_perform_action(element, "get_text")
+    else:
+        return None
+
+def get_first_deactivated_users_email_address():
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
+    element = ("xpath", '(//a[text()="Reactivate"]/ancestor::tr/td[2])[1]')
+    if check_element_exists(element):
+        return find_element_and_perform_action(element, "get_text")
+    else:
+        return None
+
+def click_first_deactivated_users_reactivate_link():
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
+    element = ("xpath", '(//a[text()="Reactivate"])[1]')
+    if check_element_exists(element):
+        find_element_and_perform_action(element, "click")
+    else:
+        logging.info("No users to reactivate")
+        attach_screenshot("No_users_available_to_reactivate")
