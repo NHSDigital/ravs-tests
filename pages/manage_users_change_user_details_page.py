0ae2d59509be4c96ef1ba58aa423c49752736a98
root <root@FA-0817>
2025-01-07 16:26:54 +0000

    Added new tests for changing user details

--- pages/manage_users_change_user_details_page.py
+++ pages/manage_users_change_user_details_page.py
@@ -9,6 +9,7 @@ LEAD_ADMINISTRATOR_PERMISSION_LEVEL_RADIO_BUTTON = ("label", "Lead administrator
 ADMINISTRATOR_PERMISSION_LEVEL_RADIO_BUTTON = ("label", "Administrator", True)
 DEACTIVATE_THE_ACCOUNT_LINK = ("role", "link", "Deactivate this account")
 BACK_TO_MANAGE_USERS_PAGE_LINK = ("role", "button", "Back")
+ADD_USER_BUTTON = ("role", "button", "Add user")
 
 def get_users_name():
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
@@ -22,32 +23,32 @@ def check_users_name_is_displayed(name):
     element = ("role", "heading", name)
     return check_element_exists(element)
 
-def change_to_yes_registered_clinician_radio_button():
+def click_change_to_yes_registered_clinician_radio_button():
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     wait_for_element_to_appear(CONTINUE_TO_CHANGE_USER_DETAILS_USER_BUTTON)
     find_element_and_perform_action(YES_REGISTERED_CLINICIAN_RADIO_BUTTON, "check")
 
-def change_to_no_registered_clinician_radio_button():
+def click_change_to_no_registered_clinician_radio_button():
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     wait_for_element_to_appear(CONTINUE_TO_CHANGE_USER_DETAILS_USER_BUTTON)
     find_element_and_perform_action(NO_REGISTERED_CLINICIAN_RADIO_BUTTON, "check")
 
-def change_to_recorder_permission_level_radio_button():
+def click_change_to_recorder_permission_level_radio_button():
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     wait_for_element_to_appear(CONTINUE_TO_CHANGE_USER_DETAILS_USER_BUTTON)
     find_element_and_perform_action(RECORDER_PERMISSION_LEVEL_RADIO_BUTTON, "check")
 
-def change_to_administrator_permission_level_radio_button():
+def click_change_to_administrator_permission_level_radio_button():
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     wait_for_element_to_appear(CONTINUE_TO_CHANGE_USER_DETAILS_USER_BUTTON)
     find_element_and_perform_action(ADMINISTRATOR_PERMISSION_LEVEL_RADIO_BUTTON, "check")
 
-def change_to_lead_administrator_permission_level_radio_button():
+def click_change_to_lead_administrator_permission_level_radio_button():
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     wait_for_element_to_appear(CONTINUE_TO_CHANGE_USER_DETAILS_USER_BUTTON)
     find_element_and_perform_action(LEAD_ADMINISTRATOR_PERMISSION_LEVEL_RADIO_BUTTON, "check")
 
-def change_to_permission_level_radio_button(permission):
+def click_change_to_permission_level_radio_button(permission):
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     wait_for_element_to_appear(CONTINUE_TO_CHANGE_USER_DETAILS_USER_BUTTON)
     element = ("label", permission, True)
@@ -62,6 +63,7 @@ def click_continue_to_change_user_details_user_button():
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     wait_for_element_to_appear(CONTINUE_TO_CHANGE_USER_DETAILS_USER_BUTTON)
     find_element_and_perform_action(CONTINUE_TO_CHANGE_USER_DETAILS_USER_BUTTON, "click")
+    wait_for_element_to_appear(ADD_USER_BUTTON)
 
 def check_deactivate_user_link_exists():
     wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
