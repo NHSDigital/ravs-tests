6fe3a10e79d05de3c0fcde4840807f7e4c2608c5
Neelima Guntupalli <neelima.guntupalli1@nhs.net>
2025-02-26 14:25:40 +0000

    Speed up tests for community pharmacy as no site

--- pages/vaccines_choose_site_page.py
+++ pages/vaccines_choose_site_page.py
@@ -5,23 +5,25 @@ CONTINUE_TO_ADD_VACCINE_BUTTON = ("role", "button", "Continue")
 SITE_LIST = (".siteRows")
 # ERROR_BATCH_ALREADY_EXISTS = "//p[text()='Error! One or more vaccine batches already exist at a site.']"
 CHOOSE_SITE_TITLE = ("role", "heading", "Choose site")
+PAGE_LOADING_ELEMENT = ("text", "Loading...Loading...")
 
 def enter_site_name(site):
-    wait_for_element_to_appear(SITE_SEARCH)
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     if check_element_exists(SITE_SEARCH): # site search will not be available for community pharmacy
         find_element_and_perform_action(SITE_SEARCH, "input_text", site)
 
 def select_site_from_list(site):
     # element = (f"//div[@class='siteRows' and contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '{site.lower()}')]")
     element = ("text", site)
-    wait_for_element_to_appear(element)
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     if check_element_exists(element):
         find_element_and_perform_action(element, "click")
 
 def click_continue_to_add_vaccine_button():
-    wait_for_element_to_appear(CONTINUE_TO_ADD_VACCINE_BUTTON)
-    find_element_and_perform_action(CONTINUE_TO_ADD_VACCINE_BUTTON, "click")
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
+    if check_element_exists(SITE_SEARCH):
+        find_element_and_perform_action(CONTINUE_TO_ADD_VACCINE_BUTTON, "click")
 
 def check_choose_site_title_exists(wait):
-    wait_for_element_to_appear(CHOOSE_SITE_TITLE)
+    wait_for_element_to_disappear(PAGE_LOADING_ELEMENT)
     return check_element_exists(CHOOSE_SITE_TITLE, wait)
