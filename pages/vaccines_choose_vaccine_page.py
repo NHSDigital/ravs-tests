47ab3d414828313d72b6fdd203cbb5730fe247c4
Neelima Guntupalli <neelima.guntupalli1@nhs.net>
2025-03-18 18:25:24 +0000

    Fixed all failing BSA tests

--- pages/vaccines_choose_vaccine_page.py
+++ pages/vaccines_choose_vaccine_page.py
@@ -1,3 +1,4 @@
+import time
 from init_helpers import *
 
 CONTINUE_TO_ENTER_BATCH_DETAILS_BUTTON=("role", "button", "Continue")
@@ -6,9 +7,9 @@ def click_continue_to_add_batch_button():
     find_element_and_perform_action(CONTINUE_TO_ENTER_BATCH_DETAILS_BUTTON, "click")
 
 def click_vaccine_radiobutton_on_add_vaccine_screen(vaccine):
-    element = ("label", vaccine.upper())
+    element = ("radio", vaccine.upper())
     find_element_and_perform_action(element, "click")
 
 def click_vaccine_type_radiobutton_on_add_vaccine_screen(vaccine_type):
-    element = ("label", vaccine_type, None, True)
+    element = ("radio", vaccine_type)
     find_element_and_perform_action(element, "click")
