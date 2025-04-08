c793685e342c877cd2114f0dc41226a80a817e24
Neelima Guntupalli <neelima.guntupalli1@nhs.net>
2025-04-08 14:31:52 +0100

    Add new pages

--- pages/select_eligibility_page.py
+++ pages/select_eligibility_page.py
@@ -1,7 +1,7 @@
 from init_helpers import *
 
 WHY_ARE_YOU_GIVING_THE_THEM_THE_VACCINE_TEXT_ELEMENT = ("role", "heading", "Why are you giving them the eligibility?")
-CONTINUE_TO_ELIGIBILITY_SCREEN = ("role", "button", "Continue")
+CONTINUE_TO_VACCINATION_LOCATION_SCREEN = ("role", "button", "Continue")
 SELECT_ELIGIBILITY_ERROR_MESSAGE_LINK = ("role", "link", "Select why you are giving them the vaccine")
 SELECT_ELIGIBILITY_ERROR_MESSAGE_TEXT = ("text", "Error: Select why you are giving them the vaccine")
 
@@ -26,5 +26,5 @@ def click_select_eligibility_error_message_link():
     wait_for_element_to_appear(WHY_ARE_YOU_GIVING_THE_THEM_THE_VACCINE_TEXT_ELEMENT)
     find_element_and_perform_action(SELECT_ELIGIBILITY_ERROR_MESSAGE_LINK, "click")
 
-def click_continue_to_choose_eligibility_screen():
-    find_element_and_perform_action(CONTINUE_TO_ELIGIBILITY_SCREEN, "click")
+def click_continue_to_choose_vaccination_location_screen():
+    find_element_and_perform_action(CONTINUE_TO_VACCINATION_LOCATION_SCREEN, "click")
