2f3708d48a253457bb085822e8328fa818e0a2e1
Neelima Guntupalli <neelima.guntupalli1@nhs.net>
2025-04-30 09:56:14 +0100

    Wait before selecting organisation

--- pages/select_organization.py
+++ pages/select_organization.py
@@ -1,12 +1,19 @@
 from init_helpers import *
 
-continue_to_home_page_button = ("role", "button", "Continue")
+CONTINUE_TO_HOME_PAGE_BUTTON = ("role", "button", "Continue")
+CHOOSE_YOUR_ORGANISATION_HEADING_TEXT_ELEMENT = ("role", "heading", "Choose your organisation")
+
+def ensure_select_organisation_text_heading_exists():
+    if not check_element_exists(CHOOSE_YOUR_ORGANISATION_HEADING_TEXT_ELEMENT):
+        wait_for_element_to_appear(CHOOSE_YOUR_ORGANISATION_HEADING_TEXT_ELEMENT)
 
 def click_continue_to_home_page_button():
-    if check_element_exists(continue_to_home_page_button):
-        find_element_and_perform_action(continue_to_home_page_button, "click")
+    ensure_select_organisation_text_heading_exists()
+    if check_element_exists(CONTINUE_TO_HOME_PAGE_BUTTON):
+        find_element_and_perform_action(CONTINUE_TO_HOME_PAGE_BUTTON, "click")
 
 def select_site(site):
+    ensure_select_organisation_text_heading_exists()
     element = ("role", "radio", site)
     if check_element_exists(element):
         find_element_and_perform_action(element, "click")
