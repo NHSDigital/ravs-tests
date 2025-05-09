1b9583351815281f1b2af45099bf90d901323ef9
Neelima Guntupalli <neelima.guntupalli1@nhs.net>
2025-05-09 12:24:16 +0100

    Setup CI

---
+++ ts-tests/cucumber.js
@@ -0,0 +1,12 @@
+let common = [
+  './e2e/features/*.feature',
+  '--require-module ts-node/register',
+  '--require ./e2e/steps/*.ts',
+  '--format progress-bar',
+  '--publish-quiet',
+  `--format-options '{"snippetInterface": "synchronous"}'`
+].join(' ');
+
+module.exports = {
+  default: common
+}
