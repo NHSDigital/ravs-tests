3bfacb9cef62d22e1469d6fc7f87de34ee37481d
Neelima Guntupalli <neelima.guntupalli1@nhs.net>
2025-05-24 17:26:51 +0100

    Reset time.sleep

--- conftest.py
+++ conftest.py
@@ -838,7 +838,7 @@ def generate_random_patient_locally(shared_data):
 
 @when(parse("I click choose vaccine button and choose the {chosen_vaccine}, {batch_number} with {batch_expiry_date} and click continue"))
 def step_choose_vaccine_and_vaccine_type(shared_data, chosen_vaccine, batch_number, batch_expiry_date):
-    time.sleep(1)
+    time.sleep(2)
     if shared_data["nhs_number"] == "9727840361":
         assert check_vaccine_history_not_available_label_element_exists() == True
     attach_screenshot("checked_vaccine_history_not_available_label_element_exists")
@@ -849,7 +849,7 @@ def step_choose_vaccine_and_vaccine_type(shared_data, chosen_vaccine, batch_numb
 
 @when(parse("I click choose vaccine button and choose the {chosen_vaccine}, {chosen_vaccine_type}, {batch_number} with {batch_expiry_date} and click continue"))
 def step_choose_vaccine_and_vaccine_type(shared_data, chosen_vaccine, chosen_vaccine_type, batch_number, batch_expiry_date):
-    time.sleep(1)
+    time.sleep(2)
     if shared_data["nhs_number"] == "9727840361":
         assert check_vaccine_history_not_available_label_element_exists() == True
     shared_data['chosen_vaccine_type'] = chosen_vaccine_type
