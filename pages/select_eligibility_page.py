d8f0efb7eb92361633f10ab97003bedca91922fa
Neelima Guntupalli <neelima.guntupalli1@nhs.net>
2025-06-05 09:39:12 +0100

    Reset BSA feature file

--- pages/select_eligibility_page.py
+++ pages/select_eligibility_page.py
@@ -15,7 +15,7 @@ def check_why_are_you_giving_vaccination_label_exists():
 
 def click_eligibility_radio_button(eligibility):
     ensure_why_are_you_giving_vaccine_heading_label_is_visible()
-    element = ("role", "radio", eligibility)
+    element = ("role", "radio", eligibility, True, {"nth": 0})
     find_element_and_perform_action(element, "click")
 
 def check_select_eligibility_error_message_link_exists():
