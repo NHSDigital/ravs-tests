9f0c854bcc2002a7db677948f0d16e28802d05b3
neelimaguntupalli1-nhs <neelima.guntupalli1@nhs.net>
2025-07-04 14:23:53 +0100

    Fix failing tests

--- test_data/models/vaccinating_clinicians.py
+++ test_data/models/vaccinating_clinicians.py
@@ -1,5 +1,4 @@
 vaccinating_clinicians = [
-    "Automated Lead Admin - neelima.guntupalli1+leadadmin_automated@nhs.net",
     "Automated Recorder - neelima.guntupalli1+recorder_automated1@nhs.net",
     "Automated Admin - neelima.guntupalli1+admin_automated1@nhs.net"
 ]
