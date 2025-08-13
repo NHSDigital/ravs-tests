52f64f752205db993b29b448a8f408faa45ea73e
Mike Monteith <mike@mikemonteith.com>
2025-08-13 12:10:11 +0100

    Add new flu vaccine types

--- test_data/models/flu_vaccine_types.py
+++ test_data/models/flu_vaccine_types.py
@@ -1,16 +1,9 @@
 flu_vaccine_types = [
-    # "Flucelvax Tetra - QIVc",
-    # "Quadrivalent Influenza vaccine - QIVe",
-    # "Quadrivalent Influvac sub - unit Tetra - QIVe",
-    # "Flucelvax Tetra - QIVc",
-    # "Supemtek - QIVr",
-    # "Fluad Tetra - aQIV",
-    # "Cell-based Quadrivalent - QIVc",
-    # "Adjuvanted Quadrivalent - aQIV"
-    "Quadrivalent Influenza Vaccine (QIVe)",
-    "Cell-based Quadrivalent Influenza Vaccine (QIVc)",
-    "Adjuvanted Quadrivalent Influenza Vaccine (aQIV)",
+    "Adjuvanted Trivalent Influenza Vaccine (aTIV)",
+    "Cell-based Trivalent Influenza Vaccine (TIVc)",
+    "Efluelda (TIV-HD)",
     "Fluenz (LAIV)",
-    "Influenza Tetra MYL (QIVe)",
-    "Quadrivalent Influenza Vaccine – High Dose (QIV-HD)",
+    "Influvac (TIVe)",
+    "Supemtek (TIVr)",
+    "Vaxigrip (TIVe)",
 ]
