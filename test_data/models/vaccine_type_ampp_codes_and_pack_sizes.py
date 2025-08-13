558aa43e5dcf86bdbddc38d5ec02570b92c33920
Mike Monteith <mike@mikemonteith.com>
2025-08-13 15:15:37 +0100

    Satisfy file formatting checks

--- test_data/models/vaccine_type_ampp_codes_and_pack_sizes.py
+++ test_data/models/vaccine_type_ampp_codes_and_pack_sizes.py
@@ -1,118 +1,118 @@
 vaccine_type_ampp_codes_pack_sizes = {
-  "vaccines": [
-    {
-      "name": "Adjuvanted Trivalent Influenza Vaccine (aTIV)",
-      "packSizes": [
+    "vaccines": [
         {
-          "size": "Single vial",
-          "amppCode": "43207811000001107"
-        },
-        {
-          "size": "10 vials",
-          "amppCode": "43207911000001102"
-        }
-      ]
-    },
-    {
-      "name": "Cell-based Trivalent Influenza Vaccine (TIVc)",
-      "packSizes": [
-        {
-          "size": "Single vial",
-          "amppCode": "43207511000001109"
-        },
-        {
-          "size": "10 vials",
-          "amppCode": "43207611000001108"
-        }
-      ]
-    },
-    {
-      "name": "Efluelda (TIV-HD)",
-      "packSizes": [
-        {
-          "size": "10 vials",
-          "amppCode": "45353611000001108"
-        }
-      ]
-    },
-    {
-      "name": "Fluenz (LAIV)",
+            "name": "Adjuvanted Trivalent Influenza Vaccine (aTIV)",
             "packSizes": [
+                {
+                    "size": "Single vial",
+                    "amppCode": "43207811000001107"
+                },
+                {
+                    "size": "10 vials",
+                    "amppCode": "43207911000001102"
+                }
+            ]
+        },
         {
-          "size": "10 vials",
-          "amppCode": "45353611000001108"
-        }
-      ]
-    },
-    {
-      "name": "Influvac (TIVe)",
+            "name": "Cell-based Trivalent Influenza Vaccine (TIVc)",
             "packSizes": [
+                {
+                    "size": "Single vial",
+                    "amppCode": "43207511000001109"
+                },
+                {
+                    "size": "10 vials",
+                    "amppCode": "43207611000001108"
+                }
+            ]
+        },
         {
-          "size": "10 vials",
-          "amppCode": "45353611000001108"
-        }
-      ]
-    },
-    {
-      "name": "Supemtek (TIVr)",
+            "name": "Efluelda (TIV-HD)",
             "packSizes": [
-        {
-          "size": "10 vials",
-          "amppCode": "45353611000001108"
-        }
-      ]
-    },
-    {
-      "name": "Vaxigrip (TIVe)",
-      "packSizes": [
-        {
-          "size": "Single vial",
-          "amppCode": "45175611000001100"
+                {
+                    "size": "10 vials",
+                    "amppCode": "45353611000001108"
+                }
+            ]
         },
         {
-          "size": "10 vials",
-          "amppCode": "45175711000001109"
-        }
-      ]
-    },
-    {
-      "name": "Spikevax JN.1",
-      "packSizes": [
-        {
-          "size": "Single vial",
-          "amppCode": "42986011000001107"
+            "name": "Fluenz (LAIV)",
+                        "packSizes": [
+                {
+                    "size": "10 vials",
+                    "amppCode": "45353611000001108"
+                }
+            ]
         },
         {
-          "size": "10 vials",
-          "amppCode": "42986211000001102"
-        }
-      ]
-    },
-    {
-      "name": "Comirnaty 30 JN.1",
-      "packSizes": [
-        {
-          "size": "Single vial",
-          "amppCode": "42986011000001107"
+            "name": "Influvac (TIVe)",
+                        "packSizes": [
+                {
+                    "size": "10 vials",
+                    "amppCode": "45353611000001108"
+                }
+            ]
         },
         {
-          "size": "10 vials",
-          "amppCode": "42986211000001102"
-        }
-      ]
-    },
-    {
-      "name": "Comirnaty 3 JN.1",
-      "packSizes": [
-        {
-          "size": "Single vial",
-          "amppCode": "42986011000001107"
+            "name": "Supemtek (TIVr)",
+                        "packSizes": [
+                {
+                    "size": "10 vials",
+                    "amppCode": "45353611000001108"
+                }
+            ]
         },
         {
-          "size": "10 vials",
-          "amppCode": "42986211000001102"
-        }
-      ]
-    },
-  ]
+            "name": "Vaxigrip (TIVe)",
+            "packSizes": [
+                {
+                    "size": "Single vial",
+                    "amppCode": "45175611000001100"
+                },
+                {
+                    "size": "10 vials",
+                    "amppCode": "45175711000001109"
+                }
+            ]
+        },
+        {
+            "name": "Spikevax JN.1",
+            "packSizes": [
+                {
+                    "size": "Single vial",
+                    "amppCode": "42986011000001107"
+                },
+                {
+                    "size": "10 vials",
+                    "amppCode": "42986211000001102"
+                }
+            ]
+        },
+        {
+            "name": "Comirnaty 30 JN.1",
+            "packSizes": [
+                {
+                    "size": "Single vial",
+                    "amppCode": "42986011000001107"
+                },
+                {
+                    "size": "10 vials",
+                    "amppCode": "42986211000001102"
+                }
+            ]
+        },
+        {
+            "name": "Comirnaty 3 JN.1",
+            "packSizes": [
+                {
+                    "size": "Single vial",
+                    "amppCode": "42986011000001107"
+                },
+                {
+                    "size": "10 vials",
+                    "amppCode": "42986211000001102"
+                }
+            ]
+        },
+    ]
 }
