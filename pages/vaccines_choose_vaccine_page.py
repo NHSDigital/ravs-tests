1691767dc75a08b8796d69fd016ac186e0503751
Mike Monteith <mike@mikemonteith.com>
2025-08-13 12:12:14 +0100

    Exact match on vaccine type

--- pages/vaccines_choose_vaccine_page.py
+++ pages/vaccines_choose_vaccine_page.py
@@ -14,9 +14,9 @@ def click_continue_to_add_batch_button():
     find_element_and_perform_action(CONTINUE_TO_ENTER_BATCH_DETAILS_BUTTON, "click")
 
 def click_vaccine_radiobutton_on_add_vaccine_screen(vaccine):
-    element = ("radio", vaccine.upper())
+    element = ("radio", vaccine, None, { "exact": True })
     find_element_and_perform_action(element, "click")
 
 def click_vaccine_type_radiobutton_on_add_vaccine_screen(vaccine_type):
-    element = ("radio", vaccine_type)
+    element = ("radio", vaccine_type, None, { "exact": True })
     find_element_and_perform_action(element, "click")
